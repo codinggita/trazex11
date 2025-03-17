d3d3f4bcd719a27da7bf8a6c255ee2cc2c569b73
Mayank Dudhatra <mayankdudhatracg@gmail.com>
2025-03-17 10:43:40 +0530

    🔧 Fixed Errors in main.jsx
    🛠 Resolved bugs causing issues in the main application file.
    
    ✅ Ensured proper rendering and routing functionality.
    
    🚀 Improved overall stability and performance.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,11 +18,8 @@ import './index.css';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
-// import Navbar from './components/Navbar/Navbar';
 import Homepage from './components/Main Home/mainhome';
-// import Footer from './components/Footer/Footer';
 import Aboutus from './components/Aboutus/Aboutus';
-// import Contents from './components/Contents/Contents';
 import Card from './components/New Contents/card';
 import Guaranteed from './components/GuaranteedPlus/Guaranteed';
 import CreateTeamsPage from './components/CreateTeams/CreateTeampage';
@@ -34,8 +31,7 @@ import MyContest from './components/MyContest/MyContest';
 import ScrennerCard from './components/Screener/ScreenerCard';
 import Wallet from './components/Wallet/Wallet';
 import Leaderboard from './components/Leaderboard/Leaderboard';
-import Loader from './components/Loader/Loader';
-import ContestDetails from './components/ContestDetails/ContestDetails';
+// import ContestDetails from './components/ContestDetails/ContestDetails';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -48,7 +44,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/aboutus" element={<Aboutus />} />
           {/* <Route path="/contents" element={<Contents />} /> */}
           <Route path="/newcontents" element={<Card />} />
-          {/* <Route path="/contentsdeatils" element={<Guaranteed />} /> */}
           <Route path="/createteams" element={<CreateTeamsPage />} />
           <Route path="/captainpage" element={<CaptainPage />} />
           <Route path="/myteams" element={<MyTeamPage />} />
@@ -58,7 +53,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/screener" element={<ScrennerCard />} />
           <Route path="/wallet" element={<Wallet />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/loader" element={<Loader />} />
           <Route path="/contestdetails/date/:date/exchange/:exchange" element={<Guaranteed />} />
 
 
